15c0c663ecad841885851d4d90848676b63fd09c
github username <github email address>
2018-08-13 17:08:06 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,8 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
-  def initialize (name)
+  attr_accessor :id, :name, :type, :db, :hp
+  def initialize (name, hp: 60)
     @name = name
+    @hp = hp
   end
 
   def self.save(name, type, db)
@@ -14,6 +15,11 @@ class Pokemon
     found_pokemon = self.new(pokemon[0][1])
     found_pokemon.id = pokemon[0][0] 
     found_pokemon.type = pokemon[0][2] 
+    found_pokemon.hp = pokemon[0][3]
     found_pokemon
   end 
+
+  def alter_hp(hit_points, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hit_points, self.id)
+  end
 end
